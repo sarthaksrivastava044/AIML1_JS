873241735c25d678b787e6d5d7abcb04404bbc68
sarthaksrivastava044 <sarthaksirvastava044@gmail.com>
2026-02-20 10:32:55 +0530

    second

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("hello world")
+// console.log("hello world")
+// document.write("hello world")
+// var num=10
+// console.log(num)
+// console.log(typeof(num))
+// num=false
+// console.log(num)
+// console.log(typeof(num))
+// num=null;
+// console.log(num)
+// console.log(typeof(num))
+// num=undefined;
+// console.log(num)
+// console.log(typeof(num))
+// num="hello worldnode";
+// console.log(num)
+// console.log(typeof(num))
+ var a=100 
+ var b=200
+ var c=50
+ var linebreak="<br>"
+ document.write("a+b+c=")
+ result =a+b+c
+ document.write(result)
+ document.write(linebreak)
+
