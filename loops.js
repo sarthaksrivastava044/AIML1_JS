4a87d1a74206b3dd3479df23c84eef12c6a61a75
sarthaksrivastava044 <sarthaksirvastava044@gmail.com>
2026-02-23 15:38:42 +0530

    day2

---
+++ loops.js
@@ -0,0 +1,16 @@
+// var count;
+// document.write("starting loop"+"<br>")
+// for(count=1;count<=10;count++){
+//     document.write("currentcount : " + count)
+//     document.write("<br>")
+// }
+// document.write("loop ended")
+
+const person ={
+    fname:"john",
+    lname:"doe",
+    age:25
+}
+for(let x in person){
+    console.log("person details: " + person[x])
+}
