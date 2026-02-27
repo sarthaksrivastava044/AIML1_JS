3351b2f2f2db142de7d8bea0907c4b66f0e72c93
sarthaksrivastava044 <sarthaksirvastava044@gmail.com>
2026-02-27 10:28:13 +0530

    objects

--- f.js
+++ f.js
@@ -1,5 +1,20 @@
-var res=mul(12,30)
-function mul(a,b){
-    return a*b;
+// var res=mul(12,30)
+// function mul(a,b){
+//     return a*b;
+// }
+// document.write(res)
+
+//normal fuction
+function add(a,b){
+    console.log(a+b);
 }
-document.write(res)
+add(10,20)
+
+//arrow function with single line parameter
+var add2=(a,b) => {
+    console.log(a+b)
+}
+add2(40,50);
+
+var add3 = (a,b) => console.log(a+b);
+add3(100,200);
